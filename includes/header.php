b43d580fc619a0d7e4bdc92993d028fdf4642299
saxtaxa <santanamnaa8@gmail.com>
2023-12-21 23:53:27 +0700

    first commit

---
+++ includes/header.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- bootstrap css -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>DataBook</title>
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+        }
+
+        .wrapper {
+            display: flex;
+            justify-content: center;
+            align-items: flex-start;
+            background-color: #f8f9fa;
+            /* add background color if needed */
+        }
+
+        .logo img {
+            width: 200px;
+            /* adjust the width as needed */
+            height: auto;
+            /* maintain aspect ratio */
+            margin-top: 20px;
+            /* adjust the margin-top as needed */
+        }
+    </style>
+</head>
+
+<body>
+    <!-- add image logo -->
+    <nav class="wrapper">
+        <div class="logo">
+            <ul>
+                <img src="assets/data_logo1.jpg" alt="Italian Trulli">
+            </ul>
+        </div>
+    </nav>
